75be0e52b6bf0d37e30e0c2dca4b9f5b1ffd1709
Jakub Dmochowski <jakub@dmochowski.co>
2020-06-16 11:22:07 +0200

    alice root geometry extracted

--- src/logic/detectorGeometry.js
+++ src/logic/detectorGeometry.js
@@ -1,29 +1,57 @@
 import * as THREE from 'three'
-import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader'
 
 class DetectorGeometry {
-  constructor(scene) {
-    let mesh = null
-    var loadingManager = new THREE.LoadingManager(() => {
-      scene.add( mesh )
-    })
-    var loader = new ColladaLoader(loadingManager)
-    loader.load('./aliceGeom.dae', (collada) => {
-      mesh = collada.scene
-      mesh.scale.x = mesh.scale.y = mesh.scale.z = 1
-      this.setPropertiesRecursively(
-        mesh,
-        {
-          layers: (l) => l.set(1),
-          material: {
-            transparent: true,
-            opacity: 0.075
+  constructor(scene = null) {
+    const file = './alice.json'
+    let objectLoader = new THREE.ObjectLoader()
+    objectLoader.load(file, (data) => {
+      const geom = data.children.find(c => c.type !== 'PerspectiveCamera')
+      const camera = data.children.find(c => c.type === 'PerspectiveCamera')
+      if(camera) {
+        camera.children.forEach(c => {
+          scene.add(c)
+        })
+      }
+      if(geom) {
+        var mergeGeometry = new THREE.Geometry()
+        var meshFaceMaterial = new THREE.MeshLambertMaterial()
+        this.mergeGeometriesRecursively(mergeGeometry, geom)
+        
+        var mesh = new THREE.Mesh(mergeGeometry, meshFaceMaterial)
+        this.setPropertiesRecursively(
+          mesh,
+          {
+            layers: (l) => l.set(1),
+            material: {
+              transparent: true,
+              opacity: 0.05,
+              wireframe: true
+            },
+            userData: this,
           },
-          userData: this,
-        },
-      )
+        )
+        scene.add(mesh)
+      }
     })
-    scene.add(mesh)
+  }
+  mergeGeometriesRecursively(mergeGeom, node) {
+    if(node instanceof THREE.Mesh) {
+      let geom = new THREE.Geometry()
+      mergeGeom.merge(geom.fromBufferGeometry(node.geometry), this.getPositionMatrix(node))
+    }
+    node.children.forEach(child => {
+      this.mergeGeometriesRecursively(mergeGeom, child)
+    })
+  }
+  getPositionMatrix(node) {
+    if(!node) return new THREE.Matrix4()
+    if(!node.parent) {
+      return node.matrix
+    } else {
+      node.updateMatrixWorld()
+      node.applyMatrix4(this.getPositionMatrix(node.parent))
+      return node.matrix
+    }
   }
   setPropertiesRecursively(obj, properties) {
     Object.keys(properties).forEach(prop => {
