ebf755624be96e4750258f9841ae5a30632598d9
Jakub Dmochowski <jakub@dmochowski.co>
2020-08-07 14:21:16 +0200

    generator of test cases

---
+++ data/testGenerator.js
@@ -0,0 +1,36 @@
+'use strict'
+const fs = require('fs')
+const uuid = require('uuid')
+let template = JSON.parse(fs.readFileSync('testGeneratorTemplate.json'))
+
+let dataSampleSizes = [1,2,5,10,20,50,100,200,500,1000,2000,5000,10000]
+
+const pickCasesFromArray = (data, numberOfElems) => {
+  let tmp = JSON.parse(JSON.stringify(data))
+  const result = []
+  for(let i = 0; i < numberOfElems; i++) {
+    if(!tmp.length) {
+      tmp = JSON.parse(JSON.stringify(data))
+    }
+    const randomIndex = Math.floor(Math.random() * tmp.length)
+    const newObj = tmp.splice(randomIndex, 1)[0]
+    newObj.uuid = uuid()
+    result.push(newObj)
+  }
+  return result
+}
+
+let generatedObjectTypes = [
+  'fTracks',
+]
+
+dataSampleSizes.forEach(size => {
+  let data = JSON.parse(JSON.stringify(template))
+  generatedObjectTypes.forEach(type => {
+    const objects = JSON.parse(fs.readFileSync(`${type}.json`))
+    data[type] = pickCasesFromArray(objects, size)
+  })
+  fs.writeFileSync(`testcase_tracks_no_${size}.json`, JSON.stringify(data))
+})
+
+
--- data/writer.js
+++ data/writer.js
@@ -1,15 +1,17 @@
 'use strict'
 const fs = require('fs')
-let file1 = fs.readFileSync('data1.json')
-let file2 = fs.readFileSync('data2.json')
+
+let number = 500
+
+let file1 = fs.readFileSync('data2.json')
+let file2 = fs.readFileSync(`testcase_tracks_no_${number}.json`)
 let idx = 0;
 let data = null;
 setInterval(
   () => {
     data = JSON.stringify(JSON.parse(idx ? file1 : file2))
     idx = (idx+1) % 2
-    fs.writeFileSync('data.json', data);
-    console.log(data)
+    fs.writeFileSync('data.json', data)
   },
-  5000
+  10000
 )
